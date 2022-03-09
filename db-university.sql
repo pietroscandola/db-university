66ed9681f6c3a270c4bfa10065115b20e4c5c4e6
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-09 15:41:58 +0100

    add sixth question

--- db-university.sql
+++ db-university.sql
@@ -30,3 +30,9 @@ FROM `exams`
 WHERE `date` = '2020-06-20'
 AND HOUR(`hour`) >= 14
 ORDER BY (`hour`)
+
+6. Selezionare tutti i corsi di laurea magistrale
+
+SELECT `name`,`level`
+FROM `degrees`
+WHERE `level` = 'magistrale'
