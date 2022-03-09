f32ef230b4d4bb37f93e2a9a58fa0f7aea9b7268
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-09 15:06:02 +0100

    add second question

--- db-university.sql
+++ db-university.sql
@@ -4,3 +4,10 @@ SELECT `name`,`surname`,`date_of_birth`
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990
 
+2. Selezionare tutti i corsi che valgono più di 10 crediti
+
+SELECT `cfu`
+FROM `courses`
+WHERE `cfu` > 10
+
+3. Selezionare tutti gli studenti che hanno più di 30 anni
