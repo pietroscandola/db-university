7601bb6924c79cdd82ddba145304de41ad4ab3dc
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-09 15:39:02 +0100

    add fifth question

--- db-university.sql
+++ db-university.sql
@@ -23,3 +23,10 @@ SELECT *
 FROM `courses`
 WHERE `period` = 'I Semestre' AND `year` = '1'
 
+5. Selezionare tutti gli appelli d esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+
+SELECT *
+FROM `exams`
+WHERE `date` = '2020-06-20'
+AND HOUR(`hour`) >= 14
+ORDER BY (`hour`)
