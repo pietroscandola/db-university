6f896ce3b34534b0234b24dd5416dc51fe16d4a3
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-10 15:32:58 +0100

    add second question

--- query_con_Join.sql
+++ query_con_Join.sql
@@ -7,3 +7,10 @@ ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
 
 /* 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze */
+
+SELECT `departments`.`name`, `degrees`.`name`
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`id` = `degrees`.`department_id`
+AND `departments`.`name` = 'Dipartimento di Neuroscienze'
